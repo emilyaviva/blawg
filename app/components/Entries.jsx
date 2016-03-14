4c0298777886d8519f72d73e39b048b4d0b54d6a
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-03-13 21:05:41 -0700

    better markup

--- app/components/Entries.jsx
+++ app/components/Entries.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import aja from 'aja'
+import axios from 'axios'
+import moment from 'moment'
 import ReactMarkdown from 'react-markdown'
 
 class Entries extends React.Component {
@@ -11,25 +12,33 @@ class Entries extends React.Component {
   }
 
   componentDidMount() {
-    aja()
-      .url('//localhost:3000/entries')
-      .type('json')
-      .on('success', entries => {
+    axios
+      .get('//localhost:3000/entries')
+      .then(res => {
         this.setState({
-          entries
+          entries: res.data
         })
       })
-      .go()
+      .catch(err => {
+        console.error(err)
+      })
   }
 
   render() {
     return (
-      <div className="entries">
+      <div className="Entries">
         {this.state.entries.length
           ? this.state.entries.map(entry =>
               <article key={entry.key} className="entry">
                 <h2>{entry.title}</h2>
+                <h4>{moment(new Date(entry.date)).format('D MMMM YYYY')}</h4>
                 <ReactMarkdown source={entry.body} />
+                <h4>
+                  {entry.tags.length
+                    ? `Tags: ${entry.tags.join(', ')}`
+                    : 'Not tagged'
+                  }
+                </h4>
               </article>
             )
           : <article className="no-entries">No entries</article>
