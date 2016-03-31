50a27ad67e4f8454f24927f2c8d371414e9117cf
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-03-31 10:37:12 -0700

    added async loading spinner

--- app/components/Entries.jsx
+++ app/components/Entries.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import axios from 'axios'
 import moment from 'moment'
 import ReactMarkdown from 'react-markdown'
-import Tags from './Tags.jsx'
+const Loader = require('react-loader')
 
 class Entries extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      loaded: false,
       entries: []
     }
   }
@@ -17,6 +18,7 @@ class Entries extends React.Component {
       .get('http://localhost:3000/entries')
       .then(res => {
         this.setState({
+          loaded: true,
           entries: res.data
         })
       })
@@ -29,22 +31,24 @@ class Entries extends React.Component {
   render() {
     return (
       <div className="Entries">
-        {this.state.entries.length
-          ? this.state.entries.map(entry =>
-              <article key={entry._id} className="entry">
-                <h2>{entry.title}</h2>
-                <h4>{moment(new Date(entry.date)).format('D MMMM YYYY')}</h4>
-                <ReactMarkdown source={entry.body} />
-                {entry.tags.map((tag, idx) =>
-                  <span key={idx}>
-                    <a href="/tags/{tag}">{tag}</a>
-                    {' '}
-                  </span>
-                )}
-              </article>
-            )
-          : <article className="no-entries">No entries</article>
-        }
+        <Loader loaded={this.state.loaded}>
+          {this.state.entries.length
+            ? this.state.entries.map(entry =>
+                <article key={entry._id} className="entry">
+                  <h2><a href={`/entries/${entry._id}`}>{entry.title}</a></h2>
+                  <h4>{moment(new Date(entry.date)).format('D MMMM YYYY')}</h4>
+                  <ReactMarkdown source={entry.body} />
+                  {entry.tags.map((tag, idx) =>
+                    <span key={idx}>
+                      <a href={`/tags/${tag}`}>{tag}</a>
+                      {' '}
+                    </span>
+                  )}
+                </article>
+              )
+            : <article className="no-entries">No entries</article>
+          }
+        </Loader>
       </div>
     )
   }
