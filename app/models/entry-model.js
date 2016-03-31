a5638de45471ac1708953757c6b165c6b4f74302
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-03-31 14:36:51 -0700

    changed static asset link

--- app/models/entry-model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-module.exports = mongoose.model('Entry', {
-  title: String,
-  date: {type: Date, default: Date.now},
-  tags: [String],
-  draft: Boolean,
-  body: String
-}, 'articles')
