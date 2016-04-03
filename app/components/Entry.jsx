b5e1a34535cf3f7335a567e409b9d23b2f7fdd2b
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-04-03 13:32:39 -0700

    added wrapper for styling

--- app/components/Entry.jsx
+++ app/components/Entry.jsx
@@ -4,23 +4,25 @@ import ReactMarkdown from 'react-markdown'
 import { Link } from 'react-router'
 
 const Entry = ({ id, date, title, body, tags }) => (
-  <article key={id} className="Entry">
-    <h2>
-      <Link to={`/entries/${id}`}>
-        {title}
-      </Link>
-    </h2>
-    <h4>{moment(new Date(date)).format('D MMMM YYYY')}</h4>
-    <ReactMarkdown source={body} />
-    {tags.map((tag, idx) =>
-      <span key={idx}>
-        <Link to={`/tags/${tag}`}>
-          {tag}
+  <div className="Entries">
+    <article key={id}>
+      <h2>
+        <Link to={`/entries/${id}`}>
+          {title}
         </Link>
-        {' '}
-      </span>
-    )}
-  </article>
+      </h2>
+      <h4>{moment(new Date(date)).format('D MMMM YYYY')}</h4>
+      <ReactMarkdown source={body} />
+      {tags.map((tag, idx) =>
+        <span key={idx}>
+          <Link to={`/tags/${tag}`}>
+            {tag}
+          </Link>
+          {' '}
+        </span>
+      )}
+    </article>
+  </div>
 )
 
 export default Entry
